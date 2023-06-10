26a9ae04c6a7bde18e0e5f915ecf3bdaf3b72043
maroon-spec <gont.jun@gmail.com>
2023-06-10 10:07:43 +0900

    up

--- chatbot_ja.py
+++ chatbot_ja.py
@@ -17,10 +17,11 @@ st.write('''
 def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
-def score_model(dataset):
+def score_model(question):
   token = st.secrets["DATABRICKS_TOKEN"]
   url = 'https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/llm-qabot-endpoint-jmaru-jpn/invocations'
   headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+  dataset = pd.DataFrame({'question':[question]})
   ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
   data_json = json.dumps(ds_dict, allow_nan=True)
   response = requests.request(method='POST', headers=headers, url=url, data=data_json)
@@ -30,10 +31,10 @@ def score_model(dataset):
   return response.json()
 
 question = st.text_input("質問")
-qa = pd.DataFrame({'question':[f"{question}"]})
+#question = pd.DataFrame({'question':[f"{qa}"]})
 
 if question != "":
-  response = score_model(qa)
+  response = score_model(question)
 
   answer = response['predictions'][0]["answer"]
   source = response['predictions'][0]["source"]
