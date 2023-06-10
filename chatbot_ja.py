38574e0b7459b5f03b8be814ab3bee47aabd47b7
maroon-spec <gont.jun@gmail.com>
2023-06-10 11:10:57 +0900

    up

--- chatbot_ja.py
+++ chatbot_ja.py
@@ -18,7 +18,6 @@ def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
 token = st.secrets["DATABRICKS_TOKEN"]
-st.write(token)
 def score_model(question):
   url = 'https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/llm-qabot-endpoint-jmaru-jpn/invocations'
   headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
